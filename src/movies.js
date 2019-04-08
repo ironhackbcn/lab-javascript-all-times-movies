2d085d575da80aac31fe3f20ace2e7c87d9ab4bd
Fabionunez <fabionunez@gmail.com>
2019-04-08 23:54:17 +0200

    Only interaction 2, a lot of problems with this practice...

--- src/movies.js
+++ src/movies.js
@@ -4,8 +4,52 @@
 
 // Get the average of all rates with 2 decimals 
 
+// var total = 0;
+// let ratesAverage = movies.map(function (element) {
+//   return element.rate;
+// });
 
-// Get the average of Drama Movies
+// for (var i = 0; i < ratesAverage.length; i++) {
+//   total += ratesAverage[i];
+// }
+
+// console.log(parseFloat(total / ratesAverage.length));
+
+
+// I was stock Trying to do it with one method
+
+
+function ratesAverage(array) {
+  let total = array.reduce((accum, element) => accum + element.rate, 0);
+  return parseFloat((total / array.length).toFixed(2));
+};
+
+console.log(ratesAverage(movies));
+
+
+
+
+
+// Get the average of Drama Movies  
+
+function dramaMoviesRate(array) {
+  // filtrar el array filtrando las que tienen una categoría drama
+  let drama = array.filter(function (element) {
+    //genre: ['Crime', 'Drama'],
+    if (array.genre.includes("Drama")) {
+      drama.push(element);
+    }
+    //number;
+  });
+  let total = drama.reduce((accum, element) => accum + element.rate, 0);
+  return parseFloat((total / array.length).toFixed(2));
+};
+
+console.log(dramaMoviesRate(movies));
+
+
+
+
 
 
 // Order by time duration, in growing order
