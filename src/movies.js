0759d36716cfb2a010d2faa121f7aeb58748b683
screeeen <miguel@screenimplosion.com>
2019-04-08 23:16:10 +0200

    2 and 3 iteration

--- src/movies.js
+++ src/movies.js
@@ -1,20 +1,39 @@
+// const data = require('data.js')
+
 /* eslint no-restricted-globals: 'off' */
-// Turn duration of the movies from hours to minutes 
+// Turn duration of the movies from hours to minutes
+
+// Get the average of all rates with 2 decimals
+var ratesAverage = function(array) {
+
+const rate = array.reduce((accumulator, currentValue) =>
+   accumulator + currentValue.rate,0)/array.length;
 
 
-// Get the average of all rates with 2 decimals 
-
+  return parseFloat(rate.toFixed(2));
+  
+};
 
 // Get the average of Drama Movies
+var dramaMoviesRate = function(array){
+  
+  const newArrayDramas = array.filter( function (element) {
+     if (element.genre.includes("Drama"))
+     return element;
+  })
+  if(newArrayDramas.length<1){
+    return;
+  }
+  var averageDramaMovies = ratesAverage(newArrayDramas);
+  console.log(newArrayDramas);
+  return averageDramaMovies;
+}
 
 
 // Order by time duration, in growing order
 
-
 // How many movies did STEVEN SPIELBERG
 
-
 // Order by title and print the first 20 titles
-
 
 // Best yearly rate average
