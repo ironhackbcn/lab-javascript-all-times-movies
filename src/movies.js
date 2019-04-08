78575dc0c8885970ccfb5aca9f2609cdfbb9ad51
Arnau Beumala Garcia <arnaubeumalagarcia@MacBook-Air-de-Arnau.local>
2019-04-09 00:54:27 +0200

    3/4 done

--- src/movies.js
+++ src/movies.js
@@ -3,18 +3,59 @@
 
 
 // Get the average of all rates with 2 decimals 
+function ratesAverage (array) {
+	let averageRate = 0;
+	array.map((movie) => {
+		averageRate += movie.rate;
+	})
+	console.log(Math.round((averageRate/array.length) * 100) / 100)
+}
 
 
 // Get the average of Drama Movies
 
+function dramaMoviesRate (array) {
+	let averageRate = 0;
+	let count = 0;
+	array.map((movie) => {
+		movie.genre.map((genre) => {
+			if (genre === 'Drama') {
+				averageRate += movie.rate;
+				count++;
+			} 	
+		})
+		
+	})
+	console.log(Math.round((averageRate/count) * 100) / 100)
+}
+
 
 // Order by time duration, in growing order
 
+function orderByDuration (array) {
+	array.sort(function(a, b){return a.duration-b.duration});
+	console.log(array);
+}
 
 // How many movies did STEVEN SPIELBERG
+
+function howManyMovies (array) {
+	let count = 0;
+	array.map((movie) => {
+		if (movie.director === 'Steven Spielberg') count++;
+	})
+
+	console.log(count);
+}
 
 
 // Order by title and print the first 20 titles
 
+function orderAlphabetically (array) {
+	array.sort(function(a, b){return a.title-b.title});
+	console.log(array.slice(0, 20));  
+}
 
 // Best yearly rate average
+
+
