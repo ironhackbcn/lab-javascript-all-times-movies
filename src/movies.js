d11eebee84f580f23848506e414d4b2f32929b8a
Jesus Cebader <zebader@hotmail.com>
2019-04-09 09:20:48 +0200

    add unfinish code for PR

--- src/movies.js
+++ src/movies.js
@@ -1,7 +1,6 @@
 /* eslint no-restricted-globals: 'off' */
 // Turn duration of the movies from hours to minutes 
 
-
 // Get the average of all rates with 2 decimals 
 function ratesAverage (movieArr) {
   const ratesTotal = movieArr.reduce(function(total,currenRate){
@@ -24,15 +23,48 @@ function dramaMoviesRate (movieArr){
   }
   return parseFloat((dramaRatesTotal/dramaTotal.length).toFixed(2));
 }
-
 console.log(dramaMoviesRate(movies))
 // Order by time duration, in growing order
 
 
 // How many movies did STEVEN SPIELBERG
+function howManyMovies(movieArr){
+  const spielDramaTotal = movieArr.filter(function(elem) {
+    return elem.genre.indexOf("Drama") > -1;});
 
-
+    const spielTotal = spielDramaTotal.filter(function(elem) {
+      return elem.director === "Steven Spielberg";
+      });
+      if(spielTotal.length > 0){
+      return `Steven Spielberg directed ${spielTotal.length} drama movies!`;
+      }
+      else{
+      return undefined;
+      }
+    
+}
+console.log(howManyMovies(movies))
 // Order by title and print the first 20 titles
-
-
+function orderAlphabetically(movieArr){
+  movieArr.sort(function (a,b){
+    if (a.title < b.title){
+      return -1;
+  }
+  
+});
+var newTitles = [];
+if(movieArr.lenght<20){
+  
+  for(let i=0;i<movieArr.lenght;i++){
+    newTitles.push(movieArr[i].title);
+  }
+  return newTitles;
+}
+else{
+for(let i=0;i<20;i++){
+  newTitles.push(movieArr[i].title);
+}
+return newTitles;
+}}
+console.log(orderAlphabetically(movies))
 // Best yearly rate average
