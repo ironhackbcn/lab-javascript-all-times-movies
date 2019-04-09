97adc2d89ff8553c36c931f91d46afe997e3b8b4
Dino Tudor <dinotudor@gmail.com>
2019-04-09 08:38:54 +0200

    Iteration 2 Done

--- src/movies.js
+++ src/movies.js
@@ -5,7 +5,32 @@
 // Get the average of all rates with 2 decimals 
 
 
-// Get the average of Drama Movies
+let arrLength = movies.filter( function (ratesLength) {
+  return ratesLength.rate;
+});
+
+//console.log(arrLength.length);
+
+let rateSum = movies.reduce(function (accumulator, score) {
+  return (accumulator + score.rate);
+}, 0);
+
+//console.log(rateSum);
+
+function ratesAverage(movies) {
+  return console.log(parseFloat(rateSum/arrLength.length));
+}
+
+/* function ratesAverage(movies) {
+  let arrLength = movies.filter( function (ratesLength) {
+    return ratesLength.rate;
+  });
+  movies.reduce(function (accumulator, score) {
+    return console.log(parseFloat(accumulator + score.rate));
+  }, 0) / arrLength.length ;
+} */
+
+ // Get the average of Drama Movies
 
 
 // Order by time duration, in growing order
