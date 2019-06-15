4f79c046d82075f76f61b33bc4759d92b50fe7c1
Jofre Moreno <jofremoreno@gmail.com>
2019-06-15 07:34:55 +0200

    done

--- src/movies.js
+++ src/movies.js
@@ -1,20 +1,26 @@
 /* eslint no-restricted-globals: 'off' */
-// Turn duration of the movies from hours to minutes 
+// Turn duration of the movies from hours to minutes
+function turnHoursToMinutes() {}
+// Get the average of all rates with 2 decimals
 
+let resultadoTotal = 0;
 
-// Get the average of all rates with 2 decimals 
-
+function ratesAverage(totalRates) {
+  for (let i = 0; i < movies.length; i++) {
+    resultadoTotal += totalRates[i].rate;
+  }
+  resultadoTotal = resultadoTotal / movies.length;
+}
 
 // Get the average of Drama Movies
-
+function dramaMoviesRate() {}
 
 // Order by time duration, in growing order
-
+function orderByDuration() {}
 
 // How many movies did STEVEN SPIELBERG
-
-
+function howManyMovies() {}
 // Order by title and print the first 20 titles
-
-
+function orderAlphabetically() {}
 // Best yearly rate average
+function bestYearAvg() {}
