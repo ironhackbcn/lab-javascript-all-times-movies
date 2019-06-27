6414b1927ddfa62473459856851e682653f1697b
Ignacy Wojdylo <ignacy.wojdylo@gmail.com>
2019-06-27 18:34:51 +0200

    finished exrecises 2 and 3, nearly finished with 5

--- src/movies.js
+++ src/movies.js
@@ -3,11 +3,44 @@
 
 
 // Get the average of all rates with 2 decimals 
+function ratesAverage(arr){
+  var sum = arr.reduce(function(acc, movie){
+    return acc += movie.rate
+  }, 0);
+  return parseFloat((sum / arr.length).toFixed(2))
+}
 
+console.log(ratesAverage(movies));
 
 // Get the average of Drama Movies
 
+function dramaMoviesRate(arr){
+  var dramaMovies = arr.filter(function(movie){
+    return movie.genre.includes("Drama")
 
+  })
+  if (dramaMovies.length === 0) {
+   return undefined;
+  }
+  var sum = dramaMovies.reduce(function(acc, movie){
+    return acc += movie.rate
+  }, 0);
+  return parseFloat((sum / dramaMovies.length).toFixed(2))
+}
+
+
+function howManyMovies(arr){
+  var dramaMovies = arr.filter(function(movie){
+    return movie.genre.includes("Drama");
+  });
+  var spielbergMovies = dramaMovies.filter(function(movie){
+    return movie.director === "Steven Spielberg";
+  });
+  if (spielbergMovies.length === 0){
+    return undefined;
+  }
+  return "Steven Spielberg directed " + spielbergMovies.length + " drama movies!";
+}
 // Order by time duration, in growing order
 
 
