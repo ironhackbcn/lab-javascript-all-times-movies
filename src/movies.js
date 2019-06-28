fbb9dc71cedc1d1285d1d76ebae0100d3da67515
JanGimenezLayola <gilajanvdm@mailfence.com>
2019-06-28 08:24:46 +0200

    ex. 2 and 3

--- src/movies.js
+++ src/movies.js
@@ -3,9 +3,26 @@
 
 
 // Get the average of all rates with 2 decimals 
-
+function ratesAverage(arr) {
+  var totalRate = arr.reduce(function(accumulator, movie){
+    accumulator.push(movie.rate);
+    return accumulator;
+  }, []);
+  var sumRates = totalRate.reduce(function(accumulator, rate){
+    return accumulator + rate;
+  }, 0)
+  var average = (sumRates / arr.length).toFixed(2);
+  return average;
+};
 
 // Get the average of Drama Movies
+function dramaMoviesRate(arr) {
+  var onlyDramaMovie = arr.filter(function(movie) {
+    return arr.genre.includes('Drama');
+  })
+  return onlyDramaMovie;
+}
+
 
 
 // Order by time duration, in growing order
