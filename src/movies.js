818c92c26e924089dc61b4b4abd8f1de5cb60e01
Jonathan Vantu <jonathanvantu@Jonathans-MacBook-Pro.local>
2019-10-24 18:28:49 +0200

    done

--- src/movies.js
+++ src/movies.js
@@ -1,3 +1,4 @@
+
 /* eslint no-restricted-globals: 'off' */
 // Turn duration of the movies from hours to minutes 
 
@@ -5,13 +6,58 @@
 // Get the average of all rates with 2 decimals 
 
 
+//ratesAverage(movies);
+function ratesAverage(array){
+    const sumOfRates = array.reduce(function( accumulator, currentElement, index) {
+        let sum = accumulator + currentElement.rate;
+        return sum;
+        }, 0);
+    let average = (sumOfRates / array.length);
+    console.log(typeof average);
+    
+    return parseFloat(average.toFixed(2));
+    }
+
+// console.log(typeof ratesAverage(movies))
+
 // Get the average of Drama Movies
 
+function dramaMoviesRate (array){
+    const dramaMovies = array.filter(function(element) {
+        return element.genre.includes('Drama');
+        })
+        if (dramaMovies.length !== 0){
+        return ratesAverage(dramaMovies);
+        }
+}
 
 // Order by time duration, in growing order
 
 
 // How many movies did STEVEN SPIELBERG
+function howManyMovies (array){
+    console.log(array.length)
+    if (array.length!==0) {
+        const spielberg = array.filter(function(element) {
+            return element.director == 'Steven Spielberg';
+        })
+        console.log(spielberg.length)
+        if (spielberg.length != 0) {
+    
+            const dramaMoviesSpielberg = spielberg.filter(function(element) {
+                return element.genre.includes('Drama');
+            })
+            return `Steven Spielberg directed ${dramaMoviesSpielberg.length} drama movies!`;
+        } else {
+            return `Steven Spielberg directed 0 drama movies!`;
+        }
+    }
+    
+}
+       console.log(howManyMovies(movies));
+
+
+
 
 
 // Order by title and print the first 20 titles
