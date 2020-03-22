32ced85c86159a24568baa93ff827295302f37e9
Àlex <alexmanye@gmail.com>
2020-03-22 23:54:36 +0100

    refactoring iterations 1 to 5

--- src/movies.js
+++ src/movies.js
@@ -1,88 +1,132 @@
-/* eslint no-restricted-globals: 'off' */
-// Turn duration of the movies from hours to minutes 
+//Iteration 1: Order by year
 
+// Review - > the second order doesn't work.
+function orderByYear(){
 
-
-// Get the average of all rates with 2 decimals 
-let arrSum = function(arr){
-    return arr.reduce(function(a,b){
-      return a + b.rate
-    }, 0);)
-  }
-  console.log (arrSum(movies).toFixed(2));
-
-
-
-// Get the average of Drama Movies 
-//Nota para AlEX: FILTER las pelis de Drama y después hacer el average
-
-function dramaMovies (movies) {
-let drama = movies.filter(function(movie){
-  return (movie.genre ===`Drama`);
-});
-console.log (drama);
+const c = movies2.sort((a,b) => {
+  if (a.year < b.year){
+    return -1;
 }
-//En replit me sale un array vacio y undefined...así que lo del average lo hago basándome en el ejercicio anterior
-
-let arrSum = function(movies){
-  return movies.reduce(function(a,movies){
-    return a + movies.genre
-  }, 0);)
-}
-console.log (arrSum(movies).toFixed(2));
-
-
-// Order by time duration, in growing order
-//Nota para Alex: utilizar SORT
-
-
-
-
-
-// How many movies did STEVEN SPIELBERG
-//Nota para Alex: utilizar FILTER
-
-function spielbergMovies (movies) {
-  let spielberg = movies.filter(function(movie){
-    return (movie.director ===`Steven Spielberg`);
-  });
-  console.log (spielberg);
-  }
-//En replit me sale tb undefined...
-
-
-// Order by title and print the first 20 titles
-//Nota para Alex: ¿SORT y crear un nuevo array mediante push con los 20 primeros mediante loop? 
-
-function orderTitle (movies){
-  let newMovies = movies.sort(function (a, b) {
-      if(a < b){
-        return -1
-      } if(a > b){
-        return 1
-      } else {
-        return 0
+  if (a.year > b.year){
+    return 1;
+} 
+  if (a.year === b.year){
+    movies2.sort((a,b) => {
+      if (a.title < b.title){
+        return -1;
       }
-      })
-
-  let titleTwenty =[];
-  for (i=0; i<19; i++){
-    titleTwenty.push(newMovies[i])
-  }
-  
-  console.log(titleTwenty);
+      if (a.title > b.title){
+        return 1;
+      }
+      return 0;
+    })
+  } return 0;
+}); return c;
 }
-//sale undefined en replit...
 
+//console.log(orderByYear());
 
+/* Solo ordenados por año:
+function orderByYear(arr){
+var moviesSortByReleaseDate = movies.sort((a,b) => {
+    if (a.year < b.year){
+      return -1;
+  }
 
+  if (a.year > b.year){
+      return 1;
+  }
+  return 0;
+}); 
+}*/
 
-// Best yearly rate average
-//Nota para Alex: ¿Se podría crear un array por año y hacer un SORT de cada array o es muy rebuscado?
+//Iteration 2: Steven Spielberg. The best?
 
+function howManyMovies(arr){
+    const spielbergMovies = movies.filter(movie => movie.director === "Steven Spielberg")
 
+  return spielbergMovies;
 
+}
+//console.log(howManyMovies());
 
+// Iteration 3: Alphabetic order
 
+const orderAlphabetically = (array) =>{
+ 
+  let titlesArray =  movies.map((movie => movie.title));
+  
+  let MoviesSorted = titlesArray.sort();
+  
+  let twentyFilmTitles = MoviesSorted.splice(0,20);
+  
+  return twentyFilmTitles;
 
+}
+//console.log(orderAlphabetically())
 
+//Iteration 4: All rates average
+
+const ratesAverage = array => {
+
+  let avRate = movies.reduce((sum, movie) => {return sum + (movie.rate)/movies.length},0);
+  
+  let avRate2dec = avRate.toFixed(2)
+  console.log(avRate2dec); 
+
+  return avRate2dec;
+
+}
+console.log(ratesAverage());
+
+//Iteration 5: Drama movies
+
+const dramaMoviesRate = array =>{
+
+let dramaMovies = movies.filter(movie => movie.genre.includes("Drama"));
+
+console.log(dramaMovies);
+
+let avRate = dramaMovies.reduce((sum, movie) => {return sum + (movie.rate)/dramaMovies.length},0);
+  
+let avRate2dec = avRate.toFixed(2)
+console.log(avRate2dec); 
+
+return avRate2dec;
+}
+
+console.log(dramaMoviesRate());
+
+//Iteration 6: Time format
+
+const turnHoursToMinutes = array => {
+
+  let totalMinutes = movies.map(movie => {
+
+    //dividimos la duración en un string de 2 con horas y minutos
+    let hoursAndMinutes = movie.duration.split(" ");
+    //console.log(hoursAndMinutes);
+
+    //A onlyMinutes le asignamos las horas y quitamos la h
+    let onlyMinutes = hoursAndMinutes[0].replace("h","");
+    //console.log(onlyMinutes);
+
+    //Quitamos el min de la parte de minutos
+    let minutesWithoutMin = hoursAndMinutes[1].replace("min","");
+    //console.log(minutesWithoutMin);
+
+    //Multiplicamos las horas por 60
+    let hoursToMinutes = parseInt(onlyMinutes*60);
+    //console.log(hoursToMinutes);
+    
+    //Sumamos horas convertidas en minutos + minutos
+
+    let durationInMinutes = hoursToMinutes + parseInt(minutesWithoutMin);
+    console.log(durationInMinutes);
+
+    return durationInMinutes;
+   }
+  );  return totalMinutes;
+}
+
+console.log(turnHoursToMinutes());
